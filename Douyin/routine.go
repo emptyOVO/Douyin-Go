89d70eac48b84868a107818b51cda6eda4664285
emptyOVO <gaoyifei040229@163.com>
2023-08-10 17:50:53 +0800

    中间件限制发布视频post请求大小

--- Douyin/routine.go
+++ Douyin/routine.go
@@ -3,7 +3,7 @@ package main
 import (
 	"Douyin/controller"
 	"Douyin/middleware"
-	limits "github.com/gin-contrib/size"
+	size "github.com/gin-contrib/size"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,15 +14,13 @@ func RouterInit(r *gin.Engine) {
 	apiRouter.GET("/user/", middleware.TokenAuth(), controller.UserInfo)
 	apiRouter.POST("/user/register/", middleware.Check(), controller.Register) //中间件检查账号密码是否合法
 	apiRouter.POST("/user/login/", controller.Login)
-	apiRouter.POST("/publish/action/", middleware.TokenAuth(), limits.RequestSizeLimiter(4<<20), controller.Publish) //limits限制整个post请求的大小，设置为3MB
+	apiRouter.POST("/publish/action/", middleware.TokenAuth(), size.RequestSizeLimiter(4<<20), controller.Publish) //size中间件限制请求大小，这里为3mb
 	apiRouter.GET("/publish/list/", middleware.TokenAuth(), controller.PublishList)
-
 	//互动接口
 	apiRouter.POST("/favorite/action/", middleware.TokenAuth(), controller.FavoriteAction)
 	apiRouter.GET("/favorite/list/", middleware.TokenAuth(), controller.FavoriteList)
 	apiRouter.POST("/comment/action/", middleware.TokenAuth(), controller.CommentAction)
 	apiRouter.GET("/comment/list/", middleware.TokenAuth(), controller.CommentList)
-
 	//社交接口
 	apiRouter.POST("/relation/action/", middleware.TokenAuth(), controller.FollowAction)
 	apiRouter.GET("/relation/follow/list/", middleware.TokenAuth(), controller.FollowList)
@@ -30,5 +28,4 @@ func RouterInit(r *gin.Engine) {
 	apiRouter.GET("/relation/friend/list/", middleware.TokenAuth(), controller.FriendList)
 	apiRouter.GET("/message/chat/", middleware.TokenAuth(), controller.MessageChat)
 	apiRouter.POST("/message/action/", middleware.TokenAuth(), controller.MessageAction)
-
 }
